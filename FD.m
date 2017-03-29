25c71a369db46b17eb01e38ab5dc23f8fc079937
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2017-03-28 23:59:52 -0600

    Q1 finish

--- FD.m
+++ FD.m
@@ -1,8 +1,34 @@
 function [B] = FD(A,l)
   [n,d] = size(A);
 
-  B = zeros(2*l,d);
-
+  B = zeros(2*l,d); 
   % fill in the rest here
+  compare=zeros(1,d);
+  for i=1:n
+      update=0;
+      for j=1:2*l
+          if B(j,:)==compare
+           B(j,:)=A(i,:);
+           update=1;
+           break
+          end
+      end    
+      if update==0   
+         [U,S,V] = svd(B);
+         sigma=diag(S);
+         sigma_l=sigma(l)^2;
+         %I = eye(d)*sigma_l;
+         for j=1:l-1;
+            S(j,j)=sqrt(S(j,j)^2-sigma_l);
+         end
+         for j=l:d;
+            S(j,j)=0;
+         end
+         B=S*V';
+      end 
+    
+      
+  end
+
 
 end
